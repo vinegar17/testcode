bec6cc5b4050291c36f97e3af9a0971e2fefe935
qc666 <649837845@qq.com>
2020-08-26 22:33:36 +0800

    第一次

--- demo.py
+++ demo.py
@@ -1,1 +1,17 @@
 print("hello word")
+print(23333)
+print(2.33)
+print({})
+print("干活干活干活"+"xixixix")
+print("哈哈哈哈")
+
+name = "连续性"
+print(name)
+res = (2*3/3+4)%3
+print(res)
+
+
+
+a = int(input("请输入",))
+a/2 == 0
+print("偶数")
